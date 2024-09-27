ed3bf50e07e19342f381deb85354953fe3a99a1a
ilovebenadryl <christian.ramirez25@compscihigh.org>
2024-09-27 02:33:13 +0000

    all tasks completed

--- index.js
+++ index.js
@@ -9,7 +9,15 @@
 // E. Delete the last fruit in the array.
 // F. Log the updated array.
 
+let fruits = ["apple", "banana", "cherry"]
 
+fruits[2]="orange"
+
+fruits.push("grape")
+
+fruits.pop
+
+console.log(fruits)
 
 // TASK 2: Objects - Create, Read, Update, Delete (CRUD)
 // A. Create an object called `student` with the keys: name, age, and grade. (add your own values)
@@ -19,18 +27,35 @@
 // E. Delete the `age` property from the object.
 // F. Log the updated student object.
 
+let student = {
+   name: "Pubert",
+   age: 22,
+   grade: "C+"
+}
 
+student.isGraduated=true
 
+delete student.age 
+
+console.log(student)
 // TASK 3: Functions - Create and Call Functions
 // A. Write a function called `calculateTotal` that takes two inputs: price and quantity.
 //    The function should return the total price by multiplying price by quantity.
 
+function calculateTotal(price, quantity){
+   return price * quantity
+}
 
 
 // B. Call the `calculateTotal` function 3 times and console log the outputs
 // First with the inputs 20 and 5
 // Second with the inputs 12 and 8
 // Third with the inputs 9 and 42
+console.log(calculateTotal(20, 5))
+
+console.log(calculateTotal(12, 8))
+
+console.log(calculateTotal(9, 42))
 
 
 /*
@@ -38,3 +63,4 @@
    At this point, commit your work with an appropriate commit message.
    Sync changes to push your commits and submit the homework!
 */
+
